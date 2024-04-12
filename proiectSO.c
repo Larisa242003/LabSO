cd456397742e42fa56d277f384979fa0565bc30b
Larisa <you@example.com>
2024-04-12 13:16:12 +0300

    modificare nr argumente

--- proiectSO.c
+++ proiectSO.c
@@ -8,7 +8,11 @@
 #include <string.h>
 #include <time.h>
 
-void listFilesRecursively(const char *basePath) {
+
+#define MAX_ARGS 10
+
+void listFilesRecursively(const char *basePath) 
+{
     char path[1000];
     struct dirent *dp;
     struct stat st;
@@ -21,20 +25,26 @@ void listFilesRecursively(const char *basePath) {
         exit(-1);
     }
 
-    while ((dp = readdir(dir)) != NULL) {
-        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+    while ((dp = readdir(dir)) != NULL) 
+    {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) 
+        {
             sprintf(path, "%s/%s", basePath, dp->d_name);
 
-            if (stat(path, &st) == 0) {
+            if (stat(path, &st) == 0) 
+            {
+                printf("File: %s\n", dp->d_name);
                 printf("Path: %s\n", path);
                 printf("Size: %ld bytes\n", st.st_size);
                 printf("Mode: %o\n", st.st_mode);
+                printf("Permissions: %o\n", st.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO));
                 printf("Last accessed: %s", ctime(&st.st_atime));
                 printf("Last modified: %s", ctime(&st.st_mtime));
                 printf("\n");
             }
 
-            if (S_ISDIR(st.st_mode)) {
+            if (S_ISDIR(st.st_mode)) 
+            {
                 listFilesRecursively(path);
             }
         }
@@ -46,42 +56,33 @@ void listFilesRecursively(const char *basePath) {
 int main(int argc,char* argv[])
 {
     struct stat st;
-    DIR *dir;
-    //struct dirent *d;
 
-    if(argc!=2)
+
+    if(argc < 2 || argc > MAX_ARGS + 1)
     {
-        perror("eroare folosire argumente");
+        perror("Eroare: Numar invalid de argumente");
         exit(-1);
     }
 
-    if(argc>2)
+    for(int i = 1; i < argc; i++)
     {
-        perror("prea multe argumente");
-        exit(-1);
+        if(stat(argv[i], &st) == -1)
+        {
+            perror("Eroare la stat");
+            printf(": %s\n", argv[i]);
+            continue;
+        }
+
+        if(!S_ISDIR(st.st_mode))
+        {
+            printf("DEBUG: Mode for %s is %o\n", argv[i], st.st_mode);
+            printf("%s nu este un director, va fi ignorat.\n", argv[i]);
+            continue;
+        }
+
+        printf("Metadatele pentru directorul: %s\n", argv[i]);
+        listFilesRecursively(argv[i]);
     }
-
-    if((dir=opendir(argv[1]))==NULL)
-    {
-        perror("argumentul nu e director");
-        exit(-1);
-    }
-
-    int status;
-    status = stat(argv[1], &st);
-    printf("%d", status);
-
-    if (!S_ISDIR(st.st_mode)) 
-    {
-        fprintf(stderr, "%s nu este un director\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
- 
-    /*while((d=readdir(dir))!=NULL){
-        printf("name: %s", d->d_name);
-    }*/
-
-    listFilesRecursively(argv[1]);
 
     return 0;
 }
