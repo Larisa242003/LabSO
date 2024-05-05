02328058d87169b884f679889b2717379fece010
Larisa242003 <165323626+Larisa242003@users.noreply.github.com>
2024-05-05 23:41:51 +0300

    Add files via upload
    aici am incercat sa fac izolarea fisierelor cu risc

--- proiectSO.c
+++ proiectSO.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 
 #define MAX_ARGS 10
@@ -48,7 +49,123 @@ void printDirectory(const char *basePath, int depth)
     closedir(dir);
 }
 
-void listFilesRecursively(const char *basePath,int outputFile) 
+void performSyntacticAnalysis(const char *filePath) 
+{
+
+    FILE *file = fopen(filePath, "r");
+    if (file == NULL) 
+    {
+        perror("Error: Unable to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[BUFFER_SIZE];
+    int lineCount = 0;
+    int wordCount = 0;
+    int charCount = 0;
+    bool foundDangerousWord = false;
+
+    while (fgets(line, BUFFER_SIZE, file) != NULL) 
+    {
+        lineCount++;
+        charCount += strlen(line);
+
+        char *token = strtok(line, " \t\n");
+        while (token != NULL) 
+        {
+            wordCount++;
+   
+            if (strstr(token, "corrupted") || strstr(token, "dangerous") || strstr(token, "risk") || strstr(token, "attack") ||strstr(token, "malware") || strstr(token, "malicious")) 
+            {
+                foundDangerousWord = true;
+                break;
+            }
+            token = strtok(NULL, " \t\n");
+        }
+
+        if (foundDangerousWord) 
+        {
+            break;
+        }
+    }
+
+    fclose(file);
+
+    printf("File: %s\n", filePath);
+    printf("Number of lines: %d\n", lineCount);
+    printf("Number of words: %d\n", wordCount);
+    printf("Number of characters: %d\n", charCount);
+
+    if (foundDangerousWord) 
+    {
+        printf("Warning: Potentially dangerous content found!\n");
+    } 
+    else 
+    {
+        printf("No dangerous content found.\n");
+    }
+}
+
+void isolateDangerousFile(const char *filePath, const char *outputDir) 
+{
+    char *fileName = strrchr(filePath, '/');
+    if (fileName == NULL) 
+    {
+        fprintf(stderr, "Error: Invalid file path\n");
+        return;
+    }
+    fileName++;
+
+    char destPath[BUFFER_SIZE];
+    snprintf(destPath, BUFFER_SIZE, "%s/%s", outputDir, fileName);
+
+    if (rename(filePath, destPath) == -1) 
+    {
+        perror("Error: Unable to move file to isolated directory");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("File isolated: %s\n", filePath);
+}
+
+
+
+void checkAndIsolateDangerousFile(const char *filePath, const char *outputDir) 
+{
+    struct stat st;
+    if (stat(filePath, &st) == -1) 
+    {
+        perror("Error: Unable to get file status");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((st.st_mode & S_IRUSR) && (st.st_mode & S_IWUSR) && (st.st_mode & S_IXUSR) &&
+        (st.st_mode & S_IRGRP) && (st.st_mode & S_IWGRP) && (st.st_mode & S_IXGRP) &&
+        (st.st_mode & S_IROTH) && (st.st_mode & S_IWOTH) && (st.st_mode & S_IXOTH)) 
+    {
+  
+        return;
+    }
+
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        perror("Error: Fork failed");
+        exit(-1);
+    } else if (pid == 0) 
+    { 
+        performSyntacticAnalysis(filePath);
+        execlp("./verify_for_malicious.sh", "verify_for_malicious.sh", filePath, NULL);
+        exit(-1);
+    } 
+        
+    isolateDangerousFile(filePath, outputDir);
+    
+}
+
+
+
+void listFilesRecursively(const char *basePath,int outputFile,const char *outputDirMalitious) 
 {
     char path[1000];
     struct dirent *dp;
@@ -117,7 +234,11 @@ void listFilesRecursively(const char *basePath,int outputFile)
 
             if (S_ISDIR(st.st_mode)) 
             {
-                listFilesRecursively(path,outputFile);
+                listFilesRecursively(path,outputFile,outputDirMalitious);
+            }
+            else
+            {
+                checkAndIsolateDangerousFile(path, outputDirMalitious);
             }
         }
     }
@@ -130,11 +251,12 @@ int main(int argc,char* argv[])
     struct stat st;
 
     char *outputDir = NULL;
+    char *outputDirMalitious=NULL;
     char *inputDirs[MAX_ARGS];
     int inputCount = 0;
 
 
-    if(argc < 3 || argc > MAX_ARGS + 1)
+    if(argc < 5 || argc > MAX_ARGS + 1)
     {
         perror("Eroare: Numar invalid de argumente");
         exit(-1);
@@ -149,13 +271,28 @@ int main(int argc,char* argv[])
                 outputDir = argv[i + 1];
                 i++;
             }
+            
             else 
             {
                 fprintf(stderr, "Eroare: Directorul de iesire lipseste pentru optiunea -o\n");
                 exit(EXIT_FAILURE);
             }
         } 
-        else 
+        else if (strcmp(argv[i], "-s") == 0) 
+        {
+            if (i + 1 < argc) 
+            {
+                outputDirMalitious = argv[i + 1];
+                i++;
+            }
+            else 
+            {
+                printf("Eroare: Directorul de iesire al fisierelor corupte lipseste pentru optiunea -s\n");
+                exit(-1);
+            }
+        }
+        
+        else
         {
             inputDirs[inputCount++] = argv[i];
         }
@@ -164,6 +301,12 @@ int main(int argc,char* argv[])
     if (outputDir == NULL) 
     {
         fprintf(stderr, "Eroare: Directorul de iesire nu este specificat. Utilizati optiunea -o\n");
+        exit(-1);
+    }
+
+    if (outputDirMalitious == NULL) 
+    {
+        fprintf(stderr, "Eroare: Directorul de iesire al fisierelor corupte nu este specificat. Utilizati optiunea -s\n");
         exit(EXIT_FAILURE);
     }
 
@@ -204,15 +347,16 @@ int main(int argc,char* argv[])
             char header[100];
             snprintf(header, 100, "Metadatele pentru directorul: %s\n", inputDirs[i]);
             write(outputFile, header, strlen(header));
-            listFilesRecursively(inputDirs[i], outputFile);
-            exit(EXIT_SUCCESS);
+            listFilesRecursively(inputDirs[i], outputFile,outputDirMalitious);
+            exit(-1);
         }
         
     }
+    
 for (int i = 0; i < inputCount; i++)
 {
     int status;
-    pid_t terminated_pid = wait(&status); // Wait for any child process to terminate
+    pid_t terminated_pid = wait(&status);
     printf("Procesul Copil %d s-a încheiat cu PID %d și cod de ieșire %d\n", i+3, terminated_pid, status);
 }
 
